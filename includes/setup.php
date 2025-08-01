22c223dcaba1b0708d897b46d18026866e920a2c
Marco Di Bella <m_dibella@icloud.com>
2025-08-01 16:04:52 +0200

    Update setup.php

--- includes/setup.php
+++ includes/setup.php
@@ -30,6 +30,7 @@ add_action( 'init', __NAMESPACE__ . '\plugin_init' );
 
 
 
+
 /**
  * The activation function for the plugin.
  *
@@ -38,16 +39,51 @@ add_action( 'init', __NAMESPACE__ . '\plugin_init' );
 
 function plugin_activation() {
 
-    $version = get_option( 'tw-sg-plugin-version' );
-
-    if ( 0 == $version ) {
-        add_option( 'tw-sg-plugin-version', PLUGIN_VERSION );
-    } else {
-        update_option( 'tw-sg-plugin-version', PLUGIN_VERSION );
+    if ( ! current_user_can( 'activate_plugins' ) ) {
+        return;
     }
 
-    add_option( 'tw-sg-scannable-post-types', [ 'post', 'page' ] );
-    add_option( 'tw-sg-filter-wp-prefixed-classes', true );
+
+    // Add table
+    global $wpdb;
+
+    $table_name    = $wpdb->prefix . TABLE_CLASSES;
+    $table_collate = $wpdb->collate;
+
+    if ( $table_name !== $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) ) {
+        $sql = "CREATE TABLE $table_name (
+            post_id bigint(20) unsigned NOT NULL default '0',
+            post_type varchar(20) NOT NULL default 'post',
+            css_classes varchar(255) NOT NULL default '',
+            PRIMARY KEY (post_id)
+            )
+            COLLATE $table_collate";
+
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+    }
+
+
+    // Add options
+    $first_run  = empty( get_option( 'tw-sg-plugin-version' ) );
+
+    if ( true === $first_run) {
+        add_option( 'tw-sg-plugin-version' );
+        add_option( 'tw-sg-filter-wp-prefixed-classes' );
+        add_option( 'tw-sg-scannable-post-types' );
+    }
+
+
+    // Set or update options
+    if ( empty( get_option( 'tw-sg-filter-wp-prefixed-classes' ) ) ) {
+        update_option( 'tw-sg-plugin-version', true );
+    }
+
+    if ( empty( get_option( 'tw-sg-scannable-post-types' ) ) ) {
+        update_option( 'tw-sg-plugin-version', ['post', 'page'] );
+    }
+
+    update_option( 'tw-sg-plugin-version', PLUGIN_VERSION );
 }
 
 register_activation_hook( __FILE__, __NAMESPACE__ . '\plugin_activation' );
@@ -55,26 +91,19 @@ register_activation_hook( __FILE__, __NAMESPACE__ . '\plugin_activation' );
 
 
 /**
- * The deactivation function for the plugin.
- *
- * @since 1.0.0
- */
-
-function plugin_deactivation() {
-    // Do something!
-}
-
-register_deactivation_hook( __FILE__, __NAMESPACE__ . '\plugin_deactivation' );
-
-
-
-/**
  * The uninstall function for the plugin.
  *
  * @since 1.0.0
+ *
+ * @todo Remove table
  */
 
 function plugin_uninstall() {
+
+    if ( ! current_user_can( 'delete_plugins' ) ) {
+        return;
+    }
+
     delete_option( 'tw-sg-scannable-post-types' );
     delete_option( 'tw-sg-filter-wp-prefixed-classes' );
     delete_option( 'tw-sg-plugin-version' );
